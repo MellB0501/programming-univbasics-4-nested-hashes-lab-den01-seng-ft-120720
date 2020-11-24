dedf1208a15a303dd2510a80b6065867607b6d9d
Mell Basham <mellbasham@gmail.com>
2020-11-24 02:33:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,7 +76,7 @@ def adding_matz
       }
     }
   programmer_hash = {:yukihiro_matsumoto => [:known_for, :languages]}
-  programmer_hash = [:known_for => "Ruby", :languages => "Lisp", "C"]
+  programmer_hash = {:known_for => "Ruby", :languages => "Lisp", "C"}
   puts programmer_hash
 end
 
